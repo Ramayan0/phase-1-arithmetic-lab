63fab73e0dc36ecfa31d4851e93b8b433505003e
hamsa <hafieekhan@gmail.com>
2022-08-18 12:43:46 +0300

    Complted Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+let random = 10;
+Math.random();
+
+let num3 = 24;
+let num4 = 10;
+const mod = num3 % num4;
+
+
+
+const max = (1, 2, 3, 4, 5, 20);
+Math.max;
